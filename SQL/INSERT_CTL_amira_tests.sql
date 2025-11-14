4ee4e6129a800acb34c23f93bda2e8272140d8af
dmellonsSLUSD <dmellons@slusd.us>
2025-11-14 08:44:21 -0800

    Added query to insert test details into CTL table

---
+++ SQL/INSERT_CTL_amira_tests.sql
@@ -0,0 +1,9 @@
+INSERT INTO CTL (ID, PT, NM) 
+VALUES 
+('AMIRA', 0, 'Amira Overall Arm'),
+('AMIRA', 1, 'Amira Decoding'),
+('AMIRA', 2, 'Amira Phonological Awareness'),
+('AMIRA', 3, 'Amira High Frequency Words'),
+('AMIRA', 4, 'Amira Background Knowledge'),
+('AMIRA', 5, 'Amira Structures and Reasoning'),
+('AMIRA', 6, 'Amira Vocabulary');
